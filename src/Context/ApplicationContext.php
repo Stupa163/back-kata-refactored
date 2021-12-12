513263d0dd2623d7ef0aed474df0d7dcca808668
Rémi Bosgaerd <remi.stupa@gmail.com>
2021-12-13 00:24:59 +0100

    Style : Linted files and typed parameters and properties for Context

--- src/Context/ApplicationContext.php
+++ src/Context/ApplicationContext.php
@@ -3,7 +3,6 @@
 namespace src\Context;
 
 use Faker\Factory;
-use src\Entity\Site;
 use src\Entity\User;
 use src\Helper\SingletonTrait;
 
@@ -11,28 +10,15 @@ class ApplicationContext
 {
     use SingletonTrait;
 
-    /**
-     * @var Site
-     */
-    private $currentSite;
-    /**
-     * @var User
-     */
-    private $currentUser;
+    private User $currentUser;
 
     protected function __construct()
     {
         $faker = Factory::create();
-        $this->currentSite = new Site($faker->randomNumber(), $faker->url);
         $this->currentUser = new User($faker->randomNumber(), $faker->firstName, $faker->lastName, $faker->email);
     }
 
-    public function getCurrentSite()
-    {
-        return $this->currentSite;
-    }
-
-    public function getCurrentUser()
+    public function getCurrentUser(): User
     {
         return $this->currentUser;
     }
