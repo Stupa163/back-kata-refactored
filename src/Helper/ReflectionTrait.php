40e846269a2e66bc87b4777b22f81ffe14d9767b
Rémi Bosgaerd <remi.stupa@gmail.com>
2021-12-14 22:43:00 +0100

    Fct : Created ReflectionTrait.php, to be used mostly for unit tests, but not only.

---
+++ src/Helper/ReflectionTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace src\Helper;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use Prophecy\Exception\Doubler\MethodNotFoundException;
+use ReflectionException;
+use ReflectionProperty;
+
+trait ReflectionTrait
+{
+    /**
+     * @throws ReflectionException
+     */
+    protected static function getPrivateProperty(string $class, string $property): ReflectionProperty
+    {
+        $reflectionProperty = new ReflectionProperty($class, $property);
+        $reflectionProperty->setAccessible(true);
+
+        return $reflectionProperty;
+    }
+
+    /**
+     * @throws ReflectionException
+     * @throws MethodNotFoundException
+     */
+    protected static function mockSingletonRepository(string $class, MockObject $mock): void
+    {
+        if (!method_exists($class, 'getInstance')) {
+            throw new MethodNotFoundException('Requested method can\'t be found', $class, 'getInstance');
+        }
+
+        (self::getPrivateProperty($class, 'instance'))->setValue($class::getInstance(), $mock);
+    }
+}
