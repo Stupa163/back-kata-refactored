45189e5d3861c8c4cceee466c018f5c7479d7e03
Rémi Bosgaerd <remi.stupa@gmail.com>
2021-12-14 22:43:51 +0100

    Test : Refactored TemplateManagerTest.php and used a provider for multiple test cases.

--- tests/TemplateManagerTest.php
+++ tests/TemplateManagerTest.php
@@ -1,78 +1,76 @@
 <?php
 
-require_once __DIR__ . '/../src/Entity/Destination.php';
-require_once __DIR__ . '/../src/Entity/Quote.php';
-require_once __DIR__ . '/../src/Entity/Site.php';
-require_once __DIR__ . '/../src/Entity/Template.php';
-require_once __DIR__ . '/../src/Entity/User.php';
-require_once __DIR__ . '/../src/Helper/SingletonTrait.php';
-require_once __DIR__ . '/../src/Context/ApplicationContext.php';
-require_once __DIR__ . '/../src/Repository/Repository.php';
-require_once __DIR__ . '/../src/Repository/DestinationRepository.php';
-require_once __DIR__ . '/../src/Repository/QuoteRepository.php';
-require_once __DIR__ . '/../src/Repository/SiteRepository.php';
-require_once __DIR__ . '/../src/TemplateManager.php';
+use PHPUnit\Framework\TestCase;
+use Prophecy\Exception\Doubler\MethodNotFoundException;
+use src\Entity\Destination;
+use src\Entity\Quote;
+use src\Entity\Site;
+use src\Entity\Template;
+use src\Entity\User;
+use src\Exceptions\InconsistentReplacementArrayException;
+use src\Helper\ReflectionTrait;
+use src\Repository\DestinationRepository;
+use src\Repository\SiteRepository;
+use src\TemplateManager;
 
-class TemplateManagerTest extends PHPUnit_Framework_TestCase
+class TemplateManagerTest extends TestCase
 {
-    /**
-     * Init the mocks
-     */
-    public function setUp()
+    use ReflectionTrait;
+
+    public function providerTestGetTemplateComputed(): array
     {
-    }
-
-    /**
-     * Closes the mocks
-     */
-    public function tearDown()
-    {
-    }
-
-    /**
-     * @test
-     */
-    public function test()
-    {
-        $faker = \Faker\Factory::create();
-
-        $expectedDestination = DestinationRepository::getInstance()->getById($faker->randomNumber());
-        $expectedUser = ApplicationContext::getInstance()->getCurrentUser();
-
-        $quote = new Quote($faker->randomNumber(), $faker->randomNumber(), $faker->randomNumber(), $faker->date());
-
-        $template = new Template(
-            1,
-            'Votre voyage avec une agence locale [quote:destination_name]',
-            "
-Bonjour [user:first_name],
-
-Merci d'avoir contacté un agent local pour votre voyage [quote:destination_name].
-
-Bien cordialement,
-
-L'équipe Evaneos.com
-www.evaneos.com
-");
-        $templateManager = new TemplateManager();
-
-        $message = $templateManager->getTemplateComputed(
-            $template,
+        return [
+            [0, '', '', '', '', ''],
+            [0, '', '', '', 'Salut tu vas bien ? :)', 'Salut tu vas bien ? :)'],
+            [1, '', 'www.atlantide.img', 'atlantide', 'Va vite sur [quote:destination_link] !', 'Va vite sur www.atlantide.img/atlantide/quote/1 !'],
+            [42, '', '', '', 'Sommaire : [quote:summary_html]', 'Sommaire : <p>42</p>'],
+            [42, '', '', '', 'Sommaire : [quote:summary]', 'Sommaire : 42'],
+            [0, '', '', 'Calabria', 'Destination : [quote:destination_name]', 'Destination : Calabria'],
+            [0, 'BERTRAND', '', '', 'Tu t\'appelles : [user:first_name]', 'Tu t\'appelles : Bertrand'],
             [
-                'quote' => $quote
+                42,
+                'BERNARD',
+                'https://www.evaneos.com',
+                'Watopia',
+                'Bonjour [user:first_name], tu pars à [quote:destination_name]. Retrouve les infos ici : [quote:destination_link] Voici ton résumé : [quote:summary], consulte également la version web : [quote:summary_html]',
+                'Bonjour Bernard, tu pars à Watopia. Retrouve les infos ici : https://www.evaneos.com/Watopia/quote/42 Voici ton résumé : 42, consulte également la version web : <p>42</p>',
             ]
-        );
+        ];
+    }
 
-        $this->assertEquals('Votre voyage avec une agence locale ' . $expectedDestination->countryName, $message->subject);
-        $this->assertEquals("
-Bonjour " . $expectedUser->firstname . ",
+    /**
+     * @dataProvider providerTestGetTemplateComputed
+     *
+     * @throws InconsistentReplacementArrayException
+     * @throws ReflectionException
+     * @throws MethodNotFoundException
+     */
+    public function testGetTemplateComputed(
+        int    $idQuote,
+        string $userFirstname,
+        string $siteUrl,
+        string $countryName,
+        string $text,
+        string $expected
+    ): void {
+        $quote = new Quote($idQuote, 1, 1);
+        $user = new User(1, $userFirstname);
+        $site = new Site(1, $siteUrl);
+        $destination = new Destination(1, $countryName);
 
-Merci d'avoir contacté un agent local pour votre voyage " . $expectedDestination->countryName . ".
+        $siteRepositoryMock = $this->createMock(SiteRepository::class);
+        $siteRepositoryMock->method('getById')->willReturn($site);
 
-Bien cordialement,
+        $destinationRepositoryMock = $this->createMock(DestinationRepository::class);
+        $destinationRepositoryMock->method('getById')->willReturn($destination);
 
-L'équipe Evaneos.com
-www.evaneos.com
-", $message->content);
+        self::mockSingletonRepository(SiteRepository::class, $siteRepositoryMock);
+        self::mockSingletonRepository(DestinationRepository::class, $destinationRepositoryMock);
+
+        $templateManager = new TemplateManager($quote, $user);
+        $template = (new Template(1,$text, $text));
+
+        self::assertSame($expected, ($templateManager->getTemplateComputed($template))->getSubject());
+        self::assertSame($expected, ($templateManager->getTemplateComputed($template))->getContent());
     }
 }
