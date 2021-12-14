12a4cc062745439e87256094b0f13ef9a2ff1d1f
Rémi Bosgaerd <remi.stupa@gmail.com>
2021-12-14 21:25:11 +0100

    Refact : Removed `$computerName` and `$conjunction` property in Destination entity

--- src/Entity/Destination.php
+++ src/Entity/Destination.php
@@ -5,15 +5,11 @@ namespace src\Entity;
 class Destination extends Entity
 {
     private string $countryName;
-    private string $conjunction;
-    private string $computerName;
 
-    public function __construct(int $id, string $countryName, string $conjunction, string $computerName)
+    public function __construct(int $id, string $countryName)
     {
         parent::__construct($id);
         $this->countryName = $countryName;
-        $this->conjunction = $conjunction;
-        $this->computerName = $computerName;
     }
 
     public function setCountryName(string $countryName): self
@@ -26,29 +22,5 @@ class Destination extends Entity
     public function getCountryName(): string
     {
         return $this->countryName;
-    }
-
-    public function setConjunction(string $conjunction): self
-    {
-        $this->conjunction = $conjunction;
-
-        return $this;
-    }
-
-    public function getConjunction(): string
-    {
-        return $this->conjunction;
-    }
-
-    public function setComputerName(string $computerName): self
-    {
-        $this->computerName = $computerName;
-
-        return $this;
-    }
-
-    public function getComputerName(): string
-    {
-        return $this->computerName;
     }
 }
